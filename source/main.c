fe40b4d49c134aacb85986da3c928b9a059144c9
amiddendorp22 <amiddendorp22@gmail.com>
2021-06-03 13:22:10 -0700

    Added player moving paddle functionality

--- source/main.c
+++ source/main.c
@@ -58,6 +58,133 @@ if (row == 0xEF && pattern == 0x01) { // Reset demo
 	return state;
 }
 
+/*
+unsigned char ballDirection = 0;  //0 is left, 1 is right
+unsigned char ballPosition = 0x00;
+enum Ball_States{ continueDirection, changeDirection};
+
+int Ball_Tick(int state)
+{
+	switch(state)
+	{
+		case(continueDirection)
+	}
+}
+
+*/
+
+enum Player_States {moveLeft, moveRight, stay};
+
+unsigned char paddleLocation = 0x1C;
+
+int Player_Tick(int state)
+{
+	unsigned char tmpA = ~PINA & 0x03; //grabs first two pins of PINA for player input up/down
+	
+	switch(state)
+	{
+		case(moveLeft):
+			if(tmpA == 0x00 || tmpA == 0x03)
+			{
+				state = stay;
+			}
+			else if(tmpA == 0x01)
+			{
+				state = moveRight;
+			}
+			else if(tmpA == 0x02)
+			{
+				state = moveLeft;
+			}
+			break;
+		case(moveRight):
+			if(tmpA == 0x00 || tmpA == 0x03)
+                        {
+                                state = stay;
+                        }
+			else if(tmpA == 0x01)
+                        {
+                                state = moveRight;
+                        }
+			else if(tmpA == 0x02)
+                        {
+                                state = moveLeft;
+                        }
+			break;
+		case(stay):
+			if(tmpA == 0x00 || tmpA == 0x03)
+                        {
+                                state = stay;
+                        }
+			else if(tmpA == 0x01)
+                        {
+                                state = moveRight;
+                        }
+			else if(tmpA == 0x02)
+                        {
+                                state = moveLeft;
+                        }
+			break;
+		default:
+			state = stay;
+			break;
+	}
+	switch(state)
+	{
+		case(moveLeft):
+			if(paddleLocation == 0xE0)
+			{
+				//Do nothing
+			}
+			else
+			{
+				paddleLocation = paddleLocation << 1;
+			}
+			break;
+		case(moveRight):
+			if(paddleLocation == 0x07)
+			{
+				//do nothing
+			}
+			else
+			{
+				paddleLocation = paddleLocation >> 1;
+			}
+			break;
+		case(stay):
+			break;
+	}
+
+	return state;
+}
+
+
+enum Display_States {display_display};
+
+
+
+int Display_Tick(int state)
+{
+	switch(state)
+	{
+		case(display_display):
+			state = display_display;
+			break;
+		default:
+			state = display_display;
+			break;
+	}
+
+	switch(state)
+	{
+		case(display_display):
+			PORTC = paddleLocation;
+			PORTD = ~(0x10);
+			break;
+	}
+
+	return state;
+}
 
 unsigned long int findGCD(unsigned long int a, unsigned long int b)
 {
@@ -84,20 +211,26 @@ typedef struct _task
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
 
-	static task task1;
-	task *tasks[] = {&task1};
+	static task task1, task2;
+	task *tasks[] = {&task1, &task2};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
 	const char start = -1;
 
 	task1.state = start;
-	task1.period = 100;
+	task1.period = 200;
 	task1.elapsedTime = task1.period;
-	task1.TickFct = &Demo_Tick;
+	task1.TickFct = &Player_Tick;
+
+	task2.state = start;
+	task2.period = 50;
+	task2.elapsedTime = task2.period;
+	task2.TickFct = &Display_Tick;
 
 	unsigned long GCD = tasks[0]->period;
 
@@ -110,6 +243,8 @@ int main(void) {
 
 	TimerSet(GCD);
 	TimerOn();
+
+	unsigned char tmpA;
 
     while (1)
     {
@@ -124,7 +259,7 @@ int main(void) {
 	}
 
 	while(!TimerFlag);
-	TimerFlag = 0;	
+	TimerFlag = 0;
     }
     return 0;
 }
